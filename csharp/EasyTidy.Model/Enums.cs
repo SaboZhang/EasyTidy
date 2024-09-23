7b788d431022573cc73f6e887ce586e1dac69985
Rick <tao993859833@live.cn>
2024-09-24 00:40:00 +0800

    refactor(csharp): 更新数据类型和UI转换器的使用
    - 更新了易用性数据类型，改用Enum.GetValues()，以改进代码的可维护性。
    - 引入新的命名空间，以支持新数据类型的使用。
    - 在UI转换中使用新引入的EnumDisplayNameConverter，以提高UI与数据类型协同工作的效率。
    - 重构了一些UI代码，以适应新引入的枚举，并且优化了代码的整洁度。

---
+++ csharp/EasyTidy.Model/Enums.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace EasyTidy.Model;
+/// <summary>
+///     备份方式
+/// </summary>
+public enum BackupType
+{
+    [Display(Name = "本地备份")]
+    Local,
+
+    [Display(Name = "WebDav")]
+    WebDav
+}
---
+++ csharp/EasyTidy/Converters/EnumToVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyTidy.Model;
+using Microsoft.UI.Xaml.Data;
+
+namespace EasyTidy.Converters;
+
+public class EnumToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        return value switch
+        {
+            BackupType backupType => backupType switch
+            {
+                BackupType.Local => Visibility.Collapsed,
+                _ => Visibility.Visible
+            },
+            _ => Visibility.Collapsed
+        };
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
