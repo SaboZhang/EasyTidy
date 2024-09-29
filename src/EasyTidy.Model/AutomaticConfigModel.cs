07a6bacc2e46a1ae252b3640a8e99b2e397f4846
Rick <tao993859833@live.cn>
2024-09-29 22:00:03 +0800

    自动化页面交互逻辑

---
+++ src/EasyTidy.Model/AutomaticConfigModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyTidy.Model;
+
+public class AutomaticConfigModel
+{
+    public bool IsFileChange { get; set;} = false;
+
+    public bool IsStartupExecution { get; set; } = false;
+
+    public bool RegularTaskRunning { get; set; } = false;
+
+    public bool OnScheduleExecution { get; set; } = false;
+}
--- src/EasyTidy/ViewModels/Automatic/AutomaticViewModel.cs
+++ src/EasyTidy/ViewModels/Automatic/AutomaticViewModel.cs
@@ -1,7 +1,9 @@
-﻿using EasyTidy.Views.ContentDialogs;
+﻿using EasyTidy.Model;
+using EasyTidy.Views.ContentDialogs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,7 +16,103 @@ public partial class AutomaticViewModel : ObservableRecipient
         this.themeService = themeService;
     }
 
+    public AutomaticViewModel()
+    {
+        
+    }
+
     public IThemeService themeService;
+
+    /// <summary>
+    ///     当前配置实例
+    /// </summary>
+    private static AutomaticConfigModel? CurConfig = Settings.AutomaticConfig;
+
+
+    /// <summary>
+    /// 文件更改时
+    /// </summary>
+    private bool _isFileChange;
+
+    public bool IsFileChange
+    {
+        get => _isFileChange;
+        set
+        {
+            if (_isFileChange != value)
+            {
+
+                _isFileChange = value;
+                CurConfig.IsFileChange = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+
+    /// <summary>
+    /// 软件启动时执行
+    /// </summary>
+    private bool _isStartupExecution;
+
+    public bool IsStartupExecution
+    {
+        get => _isStartupExecution;
+        set
+        {
+            if (_isStartupExecution != value)
+            {
+
+                _isStartupExecution = value;
+                CurConfig.IsStartupExecution = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+
+    /// <summary>
+    /// 周期执行
+    /// </summary>
+    private bool _regularTaskRunning;
+
+    public bool RegularTaskRunning
+    {
+        get => _regularTaskRunning;
+        set
+        {
+            if (_regularTaskRunning != value)
+            {
+                _regularTaskRunning = value;
+                CurConfig.RegularTaskRunning = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+
+    /// <summary>
+    /// 按计划执行
+    /// </summary>
+    private bool _onScheduleExecution;
+
+    public bool OnScheduleExecution
+    {
+
+        get => _onScheduleExecution;
+        set
+        {
+            if (_onScheduleExecution != value)
+            {
+                _onScheduleExecution = value;
+                CurConfig.OnScheduleExecution = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+
+    /// <summary>
+    /// 是否单独配置标识
+    /// </summary>
+    [ObservableProperty]
+    public bool _featureIndependentConfigFlag = false;
 
     [ObservableProperty]
     public bool isOpen = false;
@@ -52,4 +150,17 @@ public partial class AutomaticViewModel : ObservableRecipient
     {
         IsOpen = true;
     }
+
+    private void NotifyPropertyChanged([CallerMemberName] string propertyName = null, bool reDoBackupDryRun = true)
+    {
+
+        // Notify UI of property change
+        OnPropertyChanged(propertyName);
+
+        Logger.Info($"GeneralViewModel: NotifyPropertyChanged {propertyName}");
+
+        UpdateCurConfig(this);
+
+    }
+
 }
