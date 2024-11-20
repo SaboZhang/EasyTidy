fcd99cf0aad973926e7b60ed1b9e722ac7128b2d
Rick <tao993859833@live.cn>
2024-11-21 00:41:33 +0800

    refactor(Automatic): 重构自动页面布局和控件
    - 使用自定义 SettingsPageControl 和 SettingsGroup 控件替换默认设置控件
    - 更新页面布局，优化用户体验
    - 移除冗余代码，提高代码可读性和维护性

---
+++ src/EasyTidy/Views/UserControls/PageLink.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyTidy.Views.UserControls;
+
+public class PageLink
+{
+    public string Text { get; set; }
+
+    public Uri Link { get; set; }
+}
---
+++ src/EasyTidy/Views/UserControls/SettingsGroupAutomationPeer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.UI.Xaml.Automation.Peers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyTidy.Views.UserControls;
+
+public partial class SettingsGroupAutomationPeer : FrameworkElementAutomationPeer
+{
+    public SettingsGroupAutomationPeer(SettingsGroup owner)
+            : base(owner)
+    {
+    }
+
+    protected override string GetNameCore()
+    {
+        var selectedSettingsGroup = (SettingsGroup)Owner;
+        return selectedSettingsGroup.Header;
+    }
+}
